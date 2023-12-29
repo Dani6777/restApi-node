719d0fb5daa0587724785e58fa390fc4f71e7adf
Daniel Morales <daniel.morales23@outlool.cl>
2023-12-29 18:09:05 -0300

    Complete Api

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 //routes
-app.use(require('./routes/index'));
 app.use('/api/movies',require('./routes/movies'));
-
 
 //starting the server
 app.listen(app.get('port'), ()=>{
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,17 +1,52 @@
 const {Router} = require('express');
 const router = Router();
-
 const movies = require('../sample.json');
+const _ = require('underscore');
 
 
 router.get('/',(req, res)=>{
     res.json(movies);
 });
 
-
 router.post('/',(req, res)=>{
-    console.log(req.body);
-    res.send('save movie');
+    const {title,director,year,genre} = req.body;
+    if(title && director && year && genre){
+       const id = movies.length +1;
+        const newMovie={...req.body, id};
+        console.log(newMovie);
+        movies.push(newMovie);
+        res.json(movies);
+    }else{
+        res.status(500).json({error:"There was an error."});
+    }
+});
+
+router.delete('/:id',(req, res)=>{
+    const {id} = req.params;
+    _.each(movies,(movie, i)=>{
+        if(movie.id == id){
+            movies.splice(i,1);
+        }
+    });
+    res.json(movies);
+});
+
+router.put('/:id',(req,res)=>{
+    const {id} = req.params;
+    const {title,director,year,genre} = req.body;
+    if(title && director && year && genre){
+        _.each(movies,(movie, i)=>{
+            if(movie.id == id){
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.genre = genre;
+            }
+        });
+        res.json(movies);
+    }else{
+        res.status(500).json({error: "There was an error"});
+    }
 });
 
 module.exports = router;
